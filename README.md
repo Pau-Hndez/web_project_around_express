347771ec22e9a27ba7c7a546a05ea90429f76399
Paula Hernandez <phernandez.musica@gmail.com>
2026-06-08 20:27:29 -0500

    primera entrega

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tripleten web_project_around_express
+
+Proyecto enfocado en backend y llamadas al servidor
+JSON, express.js node.js
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = 3000;
+
+app.get("/users", (req, res) => {
+  fs.readFile("./data/users.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const users = JSON.parse(data);
+    res.json(users);
+  });
+});
+app.get("/cards", (req, res) => {
+  fs.readFile("./data/cards.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    const cards = JSON.parse(data);
+    res.json(cards);
+  });
+});
+app.get("/users/:id", (req, res) => {
+  fs.readFile("./data/users.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const users = JSON.parse(data);
+    const user = users.find((user) => user._id === req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "ID de usuario no encontrado",
+      });
+    }
+
+    res.json(user);
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: "Recurso solicitado no encontrado" });
+});
+
+app.listen(PORT, () => {
+  console.log(`servidor corriendo http://localhost:${PORT}`);
+});
